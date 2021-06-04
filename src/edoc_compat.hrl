3d82c35caf561a55c2c1c726643d113e4f281e00
Ulf Wiger <ulf@wiger.net>
2021-06-04 15:57:35 +0200

    fix conditional macros in edoc_compat

--- src/edoc_compat.hrl
+++ src/edoc_compat.hrl
@@ -6,11 +6,15 @@
 -define(NO_APP, no_app).
 -endif.
 
+-ifdef(OTP_RELEASE).
 -if(?OTP_RELEASE >= 24).
 -define(context, doclet_context).
 -else.
 %% Pre OTP 24 EDoc
 -define(context, context).
--endif. %% NO_APP
+-endif. %% OTP_RELEASE >= 24
+-else.
+-define(context, context).
+-endif. %% OTP_RELEASE
 
 -endif. %% __EDOC_COMPAT_HRL__
